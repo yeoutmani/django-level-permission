ef82c7e30e4dc1686621982a522b8177d6a56d9f
EL OUTMANI YASSINE <85106701+yeoutmani@users.noreply.github.com>
2024-01-25 13:06:41 +0100

    use has_perm to define rool by user

--- inventory/admin.py
+++ inventory/admin.py
@@ -3,8 +3,24 @@ from django.contrib import admin
 from .models import Product
 
 #admin.site.register(Product)
+class ReadOnlyAdminMixin:
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        if request.user.has_perm('inventory.change_product'):
+            return True
+        else:
+            return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_view_permission(self, request, obj=None):
+        return True
+
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
     list_display = ("name", )
 
     # def get_form(self, request, obj: None, **kwargs):
@@ -15,15 +31,3 @@ class ProductAdmin(admin.ModelAdmin):
     #         form.base_fields['name'].disabled = True
 
     #     return form
-    
-    def has_add_permission(self, request):
-        return False
-
-    def has_change_permission(self, request, obj=None):
-        return False
-
-    def has_delete_permission(self, request, obj=None):
-        return False
-
-    def has_view_permission(self, request, obj=None):
-        return True

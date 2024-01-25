2c48f72419e736362c021c63f7deb2bbb3f6cecb
EL OUTMANI YASSINE <85106701+yeoutmani@users.noreply.github.com>
2024-01-25 12:19:58 +0100

    if not supperuser disable name from form

--- inventory/admin.py
+++ inventory/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 
 from .models import Product
 
-admin.site.register(Product)
+#admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", )
+
+    def get_form(self, request, obj: None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+
+        if not is_superuser:
+            form.base_fields['name'].disabled = True
+
+        return form

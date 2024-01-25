9094a6725242be228eb770f5f07f109cef70a983
EL OUTMANI YASSINE <85106701+yeoutmani@users.noreply.github.com>
2024-01-25 12:51:36 +0100

    give view permission to a user

--- inventory/admin.py
+++ inventory/admin.py
@@ -7,11 +7,23 @@ from .models import Product
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("name", )
 
-    def get_form(self, request, obj: None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-        is_superuser = request.user.is_superuser
+    # def get_form(self, request, obj: None, **kwargs):
+    #     form = super().get_form(request, obj, **kwargs)
+    #     is_superuser = request.user.is_superuser
 
-        if not is_superuser:
-            form.base_fields['name'].disabled = True
+    #     if not is_superuser:
+    #         form.base_fields['name'].disabled = True
 
-        return form
+    #     return form
+    
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_view_permission(self, request, obj=None):
+        return True

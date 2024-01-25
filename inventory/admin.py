593d6cee139737c6e21e79ca156ccfdcfbb9352d
EL OUTMANI YASSINE <85106701+yeoutmani@users.noreply.github.com>
2024-01-25 16:01:42 +0100

    check if user has permission guard to view an object of a model

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,51 +1,31 @@
 from django.contrib import admin
 from .models import Product
-from django.contrib.auth.models import User,Group
-from django.contrib.auth.admin import UserAdmin
+from guardian.admin import GuardedModelAdmin
 
-admin.site.unregister(User)
-admin.site.unregister(Group)
+@admin.register(Product)
+class ProductAdmin(GuardedModelAdmin):
+    list_display = ('name', )
 
-@admin.register(User)
-class CustomUserAmin(UserAdmin):
-    def get_form(self, request, obj: None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-        is_superuser = request.user.is_superuser
+    def has_module_permission(self, request) :
+        if super().has_module_permission(request):
+            return True
 
-        if not is_superuser:
-            form.base_fields['username'].disabled = True
-            form.base_fields['is_superuser'].disabled = True
-            form.base_fields['user_permissions'].disabled = True
-            form.base_fields['groups'].disabled = True
+    def get_queryset(self, request):
+        return super().get_queryset(request)
 
-        return form
+    def has_permission(sefl, request, obj, action):
+        opts = sefl.opts
+        code_name = f'{action}_{opts.model_name}'
+        if obj:
+            return request.user.has_perm(f'{opts.app_label}.{code_name}', obj)
+        else:
+            return True
 
-#admin.site.register(Product)
-# class ReadOnlyAdminMixin:
-#     def has_add_permission(self, request):
-#         return False
-
-#     def has_change_permission(self, request, obj=None):
-#         if request.user.has_perm('inventory.change_product'):
-#             return False
-#         else:
-#             return False
-
-#     def has_delete_permission(self, request, obj=None):
-#         return False
-
-#     def has_view_permission(self, request, obj=None):
-#         return True
-
-# @admin.register(Product)
-# class ProductAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
-#     list_display = ("name", )
-
-#     # def get_form(self, request, obj: None, **kwargs):
-#     #     form = super().get_form(request, obj, **kwargs)
-#     #     is_superuser = request.user.is_superuser
-
-#     #     if not is_superuser:
-#     #         form.base_fields['name'].disabled = True
-
-#     #     return form
+    def has_view_permission(self, request, obj=None):
+        return self.has_permission(request, obj, 'view')
+    
+    def has_delete_permission(self, request, obj=None):
+        return self.has_permission(request, obj, 'delete')
+    
+    def has_change_permission(self, request, obj=None):
+        return self.has_permission(request, obj, 'change')
